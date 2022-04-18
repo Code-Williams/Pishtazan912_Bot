7c479ae198b1f5d605e80e20bbf8e83641fa3f4c
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-04-18 22:52:21 +0430

    Whatsapp All method changed and fixed

--- whatsapp_all/main.py
+++ whatsapp_all/main.py
@@ -1,8 +1,16 @@
 import utils
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
 
 time = input("Enter time [Seconds]: ")
 file_name = input("Enter file name : ")
 numbers = utils.read_excel_data(file_name)
 
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver.get("https://web.whatsapp.com")
+
+input("Scan QR code and hit enter")
+
 for number in numbers:
-    utils.send_message(number, "Hi!", time)
+    utils.send_message(number, "Hi!", time, driver)
--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -1,7 +1,6 @@
 import webbrowser, pyautogui, time, os, datetime
 import pandas as pd
 
-counter = 0
 
 def log(number, message):
     log_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -10,25 +9,32 @@ def log(number, message):
     log_file.write("\n" + f"\nNumber : {number}\nDate : {log_date}\nTime : {log_time}\nMessage : {message}\n" + ("-" * 20))
     log_file.close()
 
-def send_message(number, message, sleep_time):
-    global counter
 
-    if counter == 2:
-        os.system("pkill firefox")
-        webbrowser.open("https://google.com")
-        time.sleep(5)
-        counter = 0
+def send_message(number, message, sleep_time, driver):
+    driver.get(f"https://wa.me/{number}")
+    find_continue_button = driver.find_element_by_xpath('//*[@id="action-button"]')
 
-    webbrowser.open(f"https:/wa.me/{number}")
-    time.sleep(int(sleep_time))
-    pyautogui.write(message)
-    pyautogui.press("enter")
-    log(number, message)
-    counter += 1
+    if find_continue_button:
+        find_continue_button.click()
+
+    time.sleep(float(sleep_time))
+    
+    while True:
+        try:
+            chat_input = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
+            chat_input.click()
+            chat_input.send_keys(message)
+            break
+        except Exception as e:
+            time.sleep(3)
+
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
+
 
 def read_excel_data(excel_file):
     file_e = pd.read_excel("excel.xlsx", index_col=None, header=None)
-    numbers = []
-    for number in file_e.iterrows():
-        numbers.append("+98" + str(number[1].tolist()).replace("[","").replace("]",""))
+    numbers = ["+989121030892", "+989151502619"]
+    # for number in file_e.iterrows():
+        # numbers.append("+98" + str(number[1].tolist()).replace("[","").replace("]",""))
     return numbers
