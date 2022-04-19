75be5866f1b07ba89868f6f1b24ecfcc0fd977e7
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-04-19 23:08:59 +0430

    Random numbers fixed

--- random_num/main.py
+++ random_num/main.py
@@ -1,24 +1,44 @@
+import pandas as pd
+
 num = input("Pish shomare ra vared konid : ")
 nums = []
-if not num.startswith("09"):
-    "09" + num
 
-if not num.startswith("0"):
-    "0" + num
+if num.startswith("09"):
+   num = num.replace("09", "+989") 
 
-lenghts = 11 - len(num) - 1
+if num.startswith("9"):
+    "+98" + num
+
+if num.startswith("0098"):
+    num = num.replace("0098", "+98")
+
+lenghts = 12 - len(num)
 nums_for_loop = ""
 
 if lenghts >= 1:
-    for numbers in range(lenghts):
-        nums_for_loop += "9"
+   nums_for_loop = "9" * lenghts
 
-for numLoop in range(0, int(nums_for_loop) + 1):
+for numLoop in range(0, int("9" + nums_for_loop) + 1):
         lenghts_for_0 = len(nums_for_loop) - len(str(numLoop))
         zeros = ""
         for zero in range(lenghts_for_0 + 1):
             zeros += "0"
         
-        nums.append(num + str(zeros) + str(numLoop))
+        created_number = num + str(zeros) + str(numLoop)
+        nums.append(created_number)
+        print(f"Number {len(nums)} is : {created_number}")
 
-print(nums)
+print("Numbers lenght is : ", len(nums))
+
+save_to_excel = input("Save this to excel file ?")
+if "y" in save_to_excel.lower() and not "n" in save_to_excel.lower():
+    
+    print("Saving numbers...")
+    df = pd.DataFrame(nums)
+    writer = pd.ExcelWriter(f'{num}-numbers.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+    print("Numbers saved successfully.")
+
+else:
+    print("Save numbers to excel file cancelled [Dont enter N in last question]")
