9098d6ca0adb65df0e8ac4b17cfed6d0b7541162
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-04-20 10:49:04 +0430

    Whatsapp with custome numbers fixed

--- whatsapp_custome/main.py
+++ whatsapp_custome/main.py
@@ -1,19 +1,40 @@
-import utils
-
-numbers = []
-
-while True:
-    req = input("Enter a number or 'done' to exit: ")
-    if req.lower() != "done":
-        numbers.append(req)
-    else:
-        break
+import utils, sys
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
 
 time = input("Enter time [Seconds]: ")
 
-message_file = open("message.txt", "r")
+if time == "about":
+    print("Developer : Shayan Nasrabadi [Wil1i]\nGitHub : https://github.com/Code-Williams")
+    sys.exit()
+
+numbers = []
+while True:
+    new_number = input("Enter number ['done' for continue] : ")
+    if not new_number.lower() == "done":
+        
+        if new_number.startswith("9"):
+            new_number = new_number.replace("9", "+989")
+        elif new_number.startswith("09"):
+            new_number = new_number.replace("09", "+989")
+        elif new_number.startswith("0098"):
+            new_number = new_number.replace("0098", "+98")
+        
+        new_number.append(new_number)
+
+    else:
+        break
+
+message_file = open("message.txt", "r", encoding="UTF-8")
 message = message_file.read()
 message_file.close()
+print(f"Message is : {message}")
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver.get("https://web.whatsapp.com")
+
+input("Scan QR code and hit enter")
 
 for number in numbers:
-    utils.send_message(number, message, time)
+    utils.send_message(number, message, time, driver)
--- whatsapp_custome/utils.py
+++ whatsapp_custome/utils.py
@@ -1,34 +1,38 @@
 import webbrowser, pyautogui, time, os, datetime
 import pandas as pd
 
-counter = 0
 
 def log(number, message):
     log_date = datetime.datetime.now().strftime("%Y-%m-%d")
     log_time = datetime.datetime.now().strftime("%H:%M:%S")
-    log_file = open("log.txt", "a")
+    log_file = open(f"log-{log_date}.txt", "a")
     log_file.write("\n" + f"\nNumber : {number}\nDate : {log_date}\nTime : {log_time}\nMessage : {message}\n" + ("-" * 20))
     log_file.close()
 
-def send_message(number, message, sleep_time):
-    global counter
 
-    if counter == 2:
-        os.system("pkill firefox")
-        webbrowser.open("https://google.com")
-        time.sleep(5)
-        counter = 0
+def send_message(number, message, sleep_time, driver):
+    driver.get(f"https://web.whatsapp.com/send?phone={number}&source=&data=#")
 
-    webbrowser.open(f"https:/wa.me/{number}")
-    time.sleep(int(sleep_time))
-    pyautogui.write(message)
-    pyautogui.press("enter")
-    log(number, message)
-    counter += 1
+    time.sleep(float(sleep_time))
 
-def read_excel_data(excel_file):
-    file_e = pd.read_excel("excel.xlsx", index_col=None, header=None)
-    numbers = []
-    for number in file_e.iterrows():
-        numbers.append("+98" + str(number[1].tolist()).replace("[","").replace("]",""))
-    return numbers
+    loop_count = 0
+    is_message_sent = True
+    while True:
+        try:
+            chat_input = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
+            chat_input.click()
+            chat_input.send_keys(message)
+            print(f"Message for [ {number} ] has been sent.")
+            break
+        except Exception as e:
+            loop_count += 1
+            if loop_count >= 3:
+                print(f"Number [ {number} ] is not defined.")
+                is_message_sent = False
+                break
+            time.sleep(3)
+
+    if is_message_sent:
+        time.sleep(0.2)
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
+        time.sleep(1.5)
