1e80d9bd3bb14edcc282de4447f91fad0fc730e1
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-03 14:20:23 +0430

    Sending message in custome mode fixed

--- whatsapp_custome/utils.py
+++ whatsapp_custome/utils.py
@@ -11,28 +11,40 @@ def log(number, message):
 
 
 def send_message(number, message, sleep_time, driver):
-    driver.get(f"https://web.whatsapp.com/send?phone={number}&source=&data=#")
-
-    time.sleep(float(sleep_time))
-
-    loop_count = 0
-    is_message_sent = True
+    trying_send = 0
     while True:
         try:
-            chat_input = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
-            chat_input.click()
-            chat_input.send_keys(message)
-            print(f"Message for [ {number} ] has been sent.")
-            break
-        except Exception as e:
-            loop_count += 1
-            if loop_count >= 3:
-                print(f"Number [ {number} ] is not defined.")
-                is_message_sent = False
-                break
-            time.sleep(3)
+            driver.get(f"https://web.whatsapp.com/send?phone={number}&source=&data=#")
 
-    if is_message_sent:
-        time.sleep(0.2)
-        driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
-        time.sleep(1.5)
+            time.sleep(float(sleep_time))
+
+            loop_count = 0
+            is_message_sent = True
+            while True:
+                try:
+                    chat_input = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
+                    chat_input.click()
+                    chat_input.send_keys(message)
+                    print(f"Message for [ {number} ] has been sent.")
+                    break
+                except Exception as e:
+                    loop_count += 1
+                    if loop_count >= 3:
+                        print(f"Number [ {number} ] is not defined.")
+                        is_message_sent = False
+                        break
+                    time.sleep(3)
+
+            if is_message_sent:
+                time.sleep(0.2)
+                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
+                time.sleep(1.5)
+            break
+        except:
+            trying_send += 1
+            if trying_send == 10:
+                print(f"[ERROR] Can't send message to [ {number} ] after 10 try, skipping this number.")
+                break
+            print("[ERROR] Message not send, wait for 1 minute")
+            time.sleep(60)
+    
