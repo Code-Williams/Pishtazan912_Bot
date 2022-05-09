7054ce5a27c2ddb0d1dc6fb8aae8ecf0dde0a97f
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-09 18:09:06 +0430

    Messages routes updated

--- Pishtazan_web/routes/index.js
+++ Pishtazan_web/routes/index.js
@@ -4,4 +4,10 @@ const Router = new express.Router();
 const indexController = require("../controller/indexController");
 Router.get("/", indexController.get);
 
+const messageController = require("../controller/msgLogController")
+Router.get("/messages/log", messageController.get);
+
+// const messagesRouter = require("./messagesRoute");
+// Router.use("/messages/log" , messagesRouter);
+
 module.exports = Router;
