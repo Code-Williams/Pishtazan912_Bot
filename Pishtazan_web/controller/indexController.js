3497a0dcf15d4551158f5f696a9c1d1f2560e98c
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-11 01:51:54 +0430

    Controller connected to settings model

--- Pishtazan_web/controller/indexController.js
+++ Pishtazan_web/controller/indexController.js
@@ -1,11 +1,17 @@
 const Messages = require("../models/Message");
 const Goals = require("../models/Goals");
+const Setting = require("../models/Settings")
 
 const get = async (req, res) => {
+  let sending = await Setting.findOne({
+    where : {
+      name : "av-checking"
+    }
+  });
   const messages = await Messages.findAll();
   const goals = await Goals.findAll();
-
-  res.render("index", { messages, goals });
+  
+  res.render("index", { messages, goals, sending : sending.value });
 };
 
 module.exports = { get };
