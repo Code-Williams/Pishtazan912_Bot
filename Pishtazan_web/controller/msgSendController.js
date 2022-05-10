5ff7e86a7c7cd19d781fe861ec298c32b75d42d4
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-10 21:17:42 +0430

    post method added to send message page

--- Pishtazan_web/controller/msgSendController.js
+++ Pishtazan_web/controller/msgSendController.js
@@ -1,5 +1,37 @@
+const Message = require("../models/Message")
+const xlsx = require("node-xlsx")
+const fs = require("fs")
+
 const get = (req, res) => {
     res.render("sendMessage");
 }
 
-module.exports = {get}
+const post = async (req, res) => {
+    if(req.file && req.file.filename){
+        if(req.body.password && req.body.password == "pishtazan912"){
+
+            const dir = __dirname.replace("/controller", "") + "/public/uploads/"
+            var numbers = await xlsx.parse(dir + req.file.filename);
+            
+            for(const i of numbers[0].data){
+                if (i[0].length >= 10 && i[0].length <= 13) {
+                    await Message.create({
+                        number: i[0],
+                        message: req.body.message,
+                        stats: "pending"
+                    })
+                }
+            }
+
+        }
+
+        fs.unlink(`../public/uploads/${req.file.filename}`)
+        
+        res.redirect("/messages/send")
+        return
+    }
+
+    res.redirect("/messages/send")
+}
+
+module.exports = {get, post}
