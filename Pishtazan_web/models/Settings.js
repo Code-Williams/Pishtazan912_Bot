360c3be782e96a2916bc5ba1e64ebb7d166f12a9
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-11 01:51:35 +0430

    Settings model added

---
+++ Pishtazan_web/models/Settings.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const db = require("../configs/db");
+
+const Setting = db.define(
+  "settings",
+  {
+    id : {
+        type : DataTypes.NUMBER,
+        primaryKey : true,
+        autoIncrement : true
+    },
+    
+    name : {
+        type : DataTypes.STRING
+    },
+
+    value : {
+        type : DataTypes.STRING
+    }
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Setting;
