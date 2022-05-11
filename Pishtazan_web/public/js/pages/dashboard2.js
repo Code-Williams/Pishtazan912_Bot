43a1e4846cdb36f8b8bb2a3692654d45c36962df
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-11 16:01:32 +0430

    Browser usage added

--- Pishtazan_web/public/js/pages/dashboard2.js
+++ Pishtazan_web/public/js/pages/dashboard2.js
@@ -83,16 +83,13 @@ $(function () {
   var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
   var pieData = {
     labels: [
-      'Chrome',
-      'IE',
-      'FireFox',
-      'Safari',
-      'Opera',
-      'Navigator'
+      'Sent',
+      'Pending',
+      'Skipped'
     ],
     datasets: [
       {
-        data: [700, 500, 400, 600, 300, 100],
+        data: [parseInt(document.getElementById("sent-messages").textContent), parseInt(document.getElementById("pending").textContent), parseInt(document.getElementById("skipped").textContent)],
         backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
       }
     ]
