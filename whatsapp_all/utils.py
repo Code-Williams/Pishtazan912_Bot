6ff87879a557d1ba2546ec46294a264a51b5d611
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-21 12:21:44 +0430

    bug fix

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -40,7 +40,7 @@ def send_message(number, message, sleep_time, driver):
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
             time.sleep(0.5)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
-            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").send_keys("C:\\Users\\Administrator\\Pictures\\ranges.pdf")
+            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").click().send_keys("C:\\Users\\Administrator\\Pictures\\ranges.pdf")
             time.sleep(1.5)
             return True
     except:
