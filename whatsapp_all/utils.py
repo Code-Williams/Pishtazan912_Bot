3ea347bddf57b79d6c1e568d85691415364478be
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-21 12:13:50 +0430

    Path updated

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -40,7 +40,7 @@ def send_message(number, message, sleep_time, driver):
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
             driver.implicitly_wait(0.5)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
-            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").send_keys("C:\\Users\\Pictures\\ranges.pdf")
+            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").send_keys("C:\\Users\\Administrator\\Pictures\\ranges.pdf")
             driver.implicitly_wait(1.5)
             return True
     except:
