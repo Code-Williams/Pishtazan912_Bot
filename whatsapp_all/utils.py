45ac9999edaede17c5c086d08ce815265002910e
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-21 12:27:44 +0430

    send file maybe fixed

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -40,8 +40,10 @@ def send_message(number, message, sleep_time, driver):
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
             time.sleep(0.5)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
-            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").click().send_keys("C:\\Users\\Administrator\\Pictures\\ranges.pdf")
-            time.sleep(1.5)
+            # driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button").click()
+            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button/input").send_keys("C:\\Users\\Administrator\\Pictures\\ranges.pdf")
+            # driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
+            time.sleep(2)
             return True
     except:
         is_number_invalid = driver.find_element_by_xpath("/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]")
