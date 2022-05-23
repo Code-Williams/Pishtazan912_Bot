48dc2ac1eb033deb4a0162f0823b618a77f6a1d5
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-23 14:54:52 +0430

    bug fix

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -38,15 +38,17 @@ def send_message(number, message, sleep_time, img_path, pdf_path, driver):
         if is_message_sent:
             time.sleep(0.2)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
-            time.sleep(0.5)
+            time.sleep(1)
             # finding and sending pdf file
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
+            time.sleep(1)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button/input").send_keys(f"C:\\Users\\Administrator\\Desktop\\Pishtazan912_Bot\\Pishtazan_web\\public\\uploads\\{pdf_path}")
             time.sleep(2)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
 
             # finding and sending picture
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
+            time.sleep(1)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input").send_keys(f"C:\\Users\\Administrator\\Desktop\\Pishtazan912_Bot\\Pishtazan_web\\public\\uploads\\{img_path}")
             time.sleep(2)
             driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
