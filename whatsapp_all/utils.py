e289e7058c7d60faae64469823980c8913830b6f
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-05-23 16:30:07 +0430

    error fixed

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -52,17 +52,20 @@ def send_message(number, message, sleep_time, img_path, pdf_path, driver):
                 driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
                 time.sleep(1)
                 driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input").send_keys(f"C:\\Users\\Administrator\\Desktop\\Pishtazan912_Bot\\Pishtazan_web\\public\\uploads\\{img_path}")
-                time.sleep(3)
+                time.sleep(1)
                 driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
 
-            time.sleep(2)
+            time.sleep(1)
             return True
     except Exception as e:
         print(e)
-        is_number_invalid = driver.find_element_by_xpath("/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]")
-        if is_number_invalid.text == "Phone number shared via url is invalid.":
-            return False
-        return 'cant send'
+        try:
+            is_number_invalid = driver.find_element_by_xpath("/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]")
+            if is_number_invalid.text == "Phone number shared via url is invalid.":
+                return False
+            return 'cant send'
+        except:
+            return 'cant send'
 
 def read_excel_data(excel_file):
     file_e = pd.read_excel(excel_file, index_col=None, header=None)
