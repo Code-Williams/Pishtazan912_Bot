761a970b551017bb3796238c130c4cf60f3b97b9
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-22 23:53:17 +0430

    Bug fix

--- whatsapp_all/utils.py
+++ whatsapp_all/utils.py
@@ -1,80 +1,33 @@
 import webbrowser, pyautogui, os, datetime, time
 from selenium.common.exceptions import NoSuchElementException
-from colorama import Fore, init
 import pandas as pd
 
-init()
+def start_setup(driver):
+    driver.get("https://web.whatsapp.com")
+    input("Scan QR then enter to start.")
 
-def log(number, message):
-    log_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    log_time = datetime.datetime.now().strftime("%H:%M:%S")
-    log_file = open(f"log-{log_date}.txt", "a")
-    log_file.write("\n" + f"\nNumber : {number}\nDate : {log_date}\nTime : {log_time}\nMessage : {message}\n" + ("-" * 20))
-    log_file.close()
-
-
-def send_message(number, message, sleep_time, img_path, pdf_path, driver):
+def send_message(number, message, sleep_time, driver):
     trying_send = 0
     try:
-        driver.get(f"https://web.whatsapp.com/send?phone={number}&source=&data=#")
+        driver.get(f"https://web.whatsapp.com/send?phone={number}&source=&data=#&text={message}")
 
-        time.sleep(float(sleep_time))
+        time.sleep(int(sleep_time))
 
-        is_message_sent = False
-        try:
-            chat_input = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]")
-            if chat_input:
-                chat_input.click()
-                chat_input.send_keys(message)
-                is_message_sent = True
-            else:
-                return 'cant send'
-        except Exception as e:
-            # try:
-            #     is_number_invalid = driver.find_element_by_xpath("/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]")
-            #     if is_number_invalid and is_number_invalid.text == "Phone number shared via url is invalid.":
-            #         return False
-            #     return 'not defined'
-            # except:
-            #     return 'not defined'
-            return False
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
 
-        if is_message_sent:
-            time.sleep(0.2)
-            driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click()
-            time.sleep(1)
-            # finding and sending pdf file
-            if pdf_path and not pdf_path == "":
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
-                time.sleep(1)
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button/input").send_keys(f"C:\\Users\\Administrator\\Desktop\\Pishtazan912_Bot\\Pishtazan_web\\public\\uploads\\{pdf_path}")
-                time.sleep(1)
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
+        time.sleep(0.5)
 
-            # finding and sending picture
-            if img_path and not img_path == "":
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]").click()
-                time.sleep(1)
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input").send_keys(f"C:\\Users\\Administrator\\Desktop\\Pishtazan912_Bot\\Pishtazan_web\\public\\uploads\\{img_path}")
-                time.sleep(1)
-                driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]").click()
+        return True
 
-            time.sleep(1)
-            return True
     except Exception as e:
-        print(e)
-        # try:
-        #     is_number_invalid = driver.find_element_by_xpath("/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]")
-        #     if is_number_invalid.text == "Phone number shared via url is invalid.":
-        #         return False
-        #     return 'cant send'
-        # except:
-        #     return 'cant send'
         return False
 
 def read_excel_data(excel_file):
-    file_e = pd.read_excel(excel_file, index_col=None, header=None)
+    excel_file = pd.read_excel(excel_file, index_col=None, header=None)
+
     numbers = []
-    for number in file_e.iterrows():
+
+    for number in excel_file.iterrows():
         numbers.append("+98" + str(number[1].tolist()).replace("[","").replace("]",""))
+
     return numbers
--- whatsapp_all/whileCheck.py
+++ whatsapp_all/whileCheck.py
@@ -1,10 +1,7 @@
 import utils, sys, mysql.connector, time, config
-from colorama import Fore, init
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
-
-init()
 
 mydb = mysql.connector.connect(
     host = "localhost",
@@ -15,29 +12,21 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
-if mydb:
-    print("Connected to DataBase")
-else:
-    print("Can't connect to DataBase")
-
+print("Connected to DataBase") if mydb else print("Can't connect to DataBase")
+    
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("https://web.whatsapp.com")
+utils.start_setup(driver)
 
 def check_db():
-    cursor.execute(f"UPdATE settings SET value = 'Yes' WHERE name = 'av-checking'")
-    mydb.commit()
-
     while True:
-        print("-"*30)
         cursor.execute("SELECT * FROM messages WHERE stats = 'pending' LIMIT 1")
         res = cursor.fetchall()
 
+        if not res: break
 
-        if not res:
-            break
+        print("-"*30)
 
         while True:
-
             cursor.execute(f"UPDATE messages SET stats = 'sending' WHERE id={res[0][0]}")
             mydb.commit()
 
@@ -47,25 +36,18 @@ def check_db():
             cursor.execute("SELECT * FROM settings WHERE name = 'try time'")
             tryTime = cursor.fetchall()[0][2]
 
-            # cursor.execute("SELECT * FROM settings WHERE name = 'img'")
-            # img_path = cursor.fetchall()[0][2]
-            img_path = ""
-
-            # cursor.execute("SELECT * FROM settings WHERE name = 'pdf'")
-            # pdf_path = cursor.fetchall()[0][2]
-            pdf_path = ""
-
             id, number, message, stats, activity_time = res[0]
-            print("Start sending" + Fore.GREEN + number + Fore.RESET)
 
-            try_time = 0
-            message_sent = utils.send_message(number, message, sleepTime, img_path, pdf_path, driver)
+            print(f"Start sending {number}")
+
+            message_sent = utils.send_message(number, message, sleepTime, driver)
 
             if message_sent == True:
-                print("Message sent for number "+ Fore.GREEN + number + Fore.RESET + " is true")
+                print(f"+ Message sent for number [ {number} ]")
                 
                 cursor.execute(f"SELECT * FROM goals WHERE name = 'sent done'")
                 sent_goal = int(cursor.fetchall()[0][2]) + 1
+
                 cursor.execute(f"SELECT * FROM goals WHERE name = 'all done'")
                 all_goal = int(cursor.fetchall()[0][2]) + 1
 
@@ -76,50 +58,18 @@ def check_db():
                 mydb.commit()
                 break
 
-            elif try_time == int(tryTime):
-                print("Message sent for number " + Fore.RED + number + Fore.RESET + " is skipped")
-
-                cursor.execute(f"SELECT * FROM goals WHERE name = 'sent done'")
-                sent_goal = int(cursor.fetchall()[0][2]) + 1
-                cursor.execute(f"SELECT * FROM goals WHERE name = 'all done'")
-                all_goal = int(cursor.fetchall()[0][2]) + 1
-
-                cursor.execute(f"UPDATE messages SET stats = 'skipped' WHERE id = {id}")
-                cursor.execute(f"UPDATE goals SET goal={sent_goal} WHERE name = 'sent done'")
-                cursor.execute(f"UPDATE goals SET goal={all_goal} WHERE name = 'all done'")
-
-                mydb.commit()
-                break
-
-            elif message_sent == 'not defined':
-                print("Account for number " + Fore.RED + number + Fore.RESET + " is not defined")
-                try_time += 1
-                time.sleep(5)
-
-            elif message_sent == 'cant send':
-                print("Account for number " + Fore.RED + number + Fore.RESET + " is not loaded")
-                try_time += 1
-                time.sleep(30)
-
             elif message_sent == False:
-                print("Account for number " + Fore.RED + number + Fore.RESET + " is invalid")
+                print(f"Account for number {number} is invalid")
 
-                cursor.execute(f"SELECT * FROM goals WHERE name = 'sent done'")
-                sent_goal = int(cursor.fetchall()[0][2]) + 1
                 cursor.execute(f"SELECT * FROM goals WHERE name = 'all done'")
                 all_goal = int(cursor.fetchall()[0][2]) + 1
                 
                 cursor.execute(f"UPDATE messages SET stats = 'skipped' WHERE id = {id}")
-                cursor.execute(f"UPDATE goals SET goal={sent_goal} WHERE name = 'sent done'")
                 cursor.execute(f"UPDATE goals SET goal={all_goal} WHERE name = 'all done'")
 
                 mydb.commit()
                 break
 
-    cursor.execute(f"UPDATE settings SET value = 'No' WHERE name = 'av-checking'")
-    mydb.commit()
-
-input("Enter to start")
 while True:
     check_db()
     time.sleep(60)
